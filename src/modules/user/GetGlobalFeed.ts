46f98ee11f31d8b17d10ad0766a2b852297d8818
eddie naff <eddienaff@eddienaff.com>
2019-06-26 23:39:57 -0700

    Add global posts subscriptions

--- src/modules/user/GetGlobalFeed.ts
+++ src/modules/user/GetGlobalFeed.ts
@@ -1,17 +1,57 @@
-import { Resolver, Query, UseMiddleware } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  UseMiddleware,
+  Subscription,
+  ResolverFilterData,
+  Root
+} from "type-graphql";
 
 import { isAuth } from "../middleware/isAuth";
 import { logger } from "../middleware/logger";
-import { Post } from "../../entity/Post";
+import { Post, PostSubType } from "../../entity/Post";
+import { PostInput } from "./createPost/CreatePostInput";
+import { PostPayload } from "./CreatePostResolver";
 
 @Resolver()
-export class GetGobalPostsResolver {
+export class GetGlobalPostsResolver {
+  // @ts-ignore
+  @Subscription(type => PostSubType, {
+    // the `payload` and `args` are available in the destructured
+    // object below `{args, context, payload}`
+    topics: ({ context }) => {
+      // @ts-ignore
+
+      if (!context.userId) {
+        throw new Error("not authed");
+      }
+      return "POSTS_GLOBAL";
+    },
+
+    // @ts-ignore
+    filter: ({ payload, context }: ResolverFilterData<Post, PostInput>) => {
+      // * Always return a boolean or Promise<boolean> here
+      // in future we'd probably sort
+      // for regionality or something
+      // business-y.
+      return true;
+    }
+  })
+  // this is the actual class method that activates?
+  // the subscribe. I'm not really sure how this part
+  // works besides accessing the root query to add a
+  // resolver method. For now I don't transform
+  // anything here
+  globalPosts(@Root() postPayload: PostPayload) {
+    return { ...postPayload };
+  }
+
   @UseMiddleware(isAuth, logger)
   @Query(() => [Post], {
-    name: "getGobalPosts",
+    name: "getGlobalPosts",
     nullable: true
   })
-  async getGobalPosts(): Promise<any> {
+  async getGlobalPosts(): Promise<any> {
     const findOptions = {
       where: {},
       relations: ["images", "user"]
