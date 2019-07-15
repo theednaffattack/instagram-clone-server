dbd4a52613725fca7c255f935e0927592a8740ab
eddie naff <eddienaff@eddienaff.com>
2019-07-15 16:16:40 -0700

    code cleanup - should consider for deletion

--- src/modules/messages/GetAllMyMessages.ts
+++ src/modules/messages/GetAllMyMessages.ts
@@ -9,14 +9,7 @@ export class GetAllMessagesResolver {
   // @ts-ignore
   @Query(type => User, { nullable: true })
   async getAllMyMessages(@Ctx() context: MyContext) {
-    // const myMessages = await Message.createQueryBuilder("message")
-    //   .leftJoinAndMapMany("message.mappedMessages", "message.user", "user")
-    //   .where("user.id = :id", { id: context.userId })
-    //   .getMany();
-
     const myUser = await User.createQueryBuilder("user")
-      //   .leftJoinAndSelect("user.messages", "message")
-      //   .leftJoinAndSelect("message.sentBy", "sentBy")
 
       .leftJoinAndMapMany(
         "user.mappedMessages",
@@ -29,20 +22,10 @@ export class GetAllMessagesResolver {
       .where("user.id = :id", { id: context.userId })
       .groupBy("user.id")
       .addGroupBy("message.id")
-      //   .addGroupBy("user.firstName")
       .addGroupBy("sentBy.id")
       .addGroupBy("m_user.id")
-      //   .addGroupBy("sentBy.firstName")
-      .orderBy({ "message.createdAt": "DESC" }) // "sentBy.firstName": "ASC",
-      //   .addGroupBy("message.sentBy.id")
+      .orderBy({ "message.created_at": "DESC" }) // "sentBy.firstName": "ASC",
       .getOne();
-
-    console.log("myUser.messages", myUser);
-    console.log("myUser.messages", myUser!.messages);
-    console.log("myUser.messages", myUser!.sent_messages);
-    // console.log("myMessages", myMessages);
-    // console.log("myMessages[0]", myMessages[0]);
-    // console.log("myMessagess[0].mappedMessages", myMessages[0].mappedMessages);
 
     return myUser;
   }
