7c5a6fc918fc67b37d8b0d707412c0b38e4c0c73
eddie naff <eddienaff@eddienaff.com>
2019-08-08 19:41:21 -0700

    Change select to search array of invitees in postgres

--- src/modules/messages/GetMessageThreads.ts
+++ src/modules/messages/GetMessageThreads.ts
@@ -1,4 +1,5 @@
 import { Resolver, Query, Ctx, UseMiddleware } from "type-graphql";
+// import util from "util";
 
 import { MyContext } from "../../types/MyContext";
 import { Thread } from "../../entity/Thread";
@@ -17,7 +18,8 @@ export class GetMessageThreadsResolver {
       .leftJoinAndSelect("message.images", "image")
       .leftJoinAndSelect("thread.invitees", "userB")
 
-      .where("userB.id = :id", { id: context.userId })
+      // .where("userB.id = :id", { id: context.userId }) // old?
+      .where("userB.id IN (:...ids)", { ids: [context.userId] })
       .orderBy("message.created_at", "ASC")
       .getMany();
 
@@ -29,20 +31,25 @@ export class GetMessageThreadsResolver {
     let cache: any = [];
 
     qThreads.forEach(thread => {
-      const sortedMessages = thread.messages.sort(function(a, b) {
-        return a.created_at.getTime() - b.created_at.getTime(); // ASC
-        // return b.created_at.getTime() - a.created_at.getTime(); // DESC
-      });
-      const copyThread = {
-        ...thread,
-        messages: [...sortedMessages]
-      };
-      cache.push(copyThread);
+      if (thread.messages) {
+        const sortedMessages = thread.messages.sort(function(a, b) {
+          return a.created_at.getTime() - b.created_at.getTime(); // ASC
+          // return b.created_at.getTime() - a.created_at.getTime(); // DESC
+        });
+        const copyThread = {
+          ...thread,
+          messages: [...sortedMessages]
+        };
+        cache.push(copyThread);
+      }
+      return;
     });
 
     let newData = {
       getMessageThreads: [...cache]
     };
+
+    // console.log(util.inspect(newData, false, 4));
 
     return newData.getMessageThreads;
   }
