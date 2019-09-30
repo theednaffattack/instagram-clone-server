18c948084155841c5ce23bdcf023687a501d188e
eddie naff <eddienaff@eddienaff.com>
2019-09-30 15:11:53 -0700

    Remove casting for env variables

--- src/production_ormconfig.ts
+++ src/production_ormconfig.ts
@@ -3,15 +3,19 @@ import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConne
 const isLoggingFalse = process.env.TYPEORM_LOGGING === "false";
 const isSynchronizeTrue = process.env.TYPEORM_SYNCHRONIZE === "true";
 
+const getPortOrSetDefault = process.env.TYPEORM_PORT
+  ? parseInt(process.env.TYPEORM_PORT)
+  : 5432;
+
 export const productionOrmConfig: PostgresConnectionOptions = {
   name: "staging",
   type: "postgres",
-  host: process.env.TYPEORM_HOST!,
-  port: parseInt(process.env.TYPEORM_PORT!),
+  host: process.env.TYPEORM_HOST,
+  port: getPortOrSetDefault,
   ssl: true,
-  username: process.env.TYPEORM_USERNAME!,
-  password: process.env.TYPEORM_PASSWORD!,
-  database: process.env.TYPEORM_DATABASE!,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
   logging: isLoggingFalse,
   synchronize: isSynchronizeTrue,
   entities: ["dist/entity/*.*"],
