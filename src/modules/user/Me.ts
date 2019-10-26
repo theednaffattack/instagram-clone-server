c5c0238c525a95197c31ebb77161a78b0e37634f
eddie naff <eddienaff@eddienaff.com>
2019-10-25 19:33:54 -0700

    Add auth middleware to Me resolver

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -1,10 +1,12 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, UseMiddleware } from "type-graphql";
 
 import { User } from "../../entity/User";
 import { MyContext } from "../../types/MyContext";
+import { isAuth } from "../middleware/isAuth";
 
 @Resolver()
 export class MeResolver {
+  @UseMiddleware(isAuth)
   @Query(() => User, { nullable: true })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     // if we can't find a userId on the current session
