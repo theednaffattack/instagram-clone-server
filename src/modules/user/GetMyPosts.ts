658703fc2726474356956245412eee85d77e0fbd
eddie naff <eddienaff@eddienaff.com>
2019-10-25 19:33:36 -0700

    Improve subs cor GetMyPosts

--- src/modules/user/GetMyPosts.ts
+++ src/modules/user/GetMyPosts.ts
@@ -1,14 +1,134 @@
-import { Resolver, Query, UseMiddleware, Ctx } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  UseMiddleware,
+  Ctx,
+  Field,
+  ID,
+  ObjectType,
+  Subscription,
+  ResolverFilterData,
+  InputType,
+  Root,
+  registerEnumType
+} from "type-graphql";
+import util from "util";
 
 import { isAuth } from "../middleware/isAuth";
 import { logger } from "../middleware/logger";
 import { MyContext } from "../../types/MyContext";
 import { User } from "../../entity/User";
 import { Post } from "../../entity/Post";
+import { Image } from "../../entity/Image";
+
+/**
+ * @type {Enum} Action - CRUD action helper
+ *
+ * ```
+ * enum Action {
+ *   Create = "CREATE",
+ *   Read = "READ",
+ *   Update = "UPDATE",
+ *   Delete = "DELETE"
+ * }
+ * ```
+ */
+enum Action {
+  Create = "CREATE",
+  Read = "READ",
+  Update = "UPDATE",
+  Delete = "DELETE"
+}
+
+registerEnumType(Action, {
+  name: "Action", // this one is mandatory
+  description: "An Enum to help with CRUD operations" // this one is optional
+});
+
+@InputType()
+export class PostSubscriptionInput {
+  @Field()
+  text: string;
+
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field(() => ID)
+  user: string;
+
+  @Field(() => [String], { nullable: "itemsAndList" })
+  images: string[];
+}
+
+@ObjectType()
+export class HandlePostPayload {
+  @Field(() => Boolean)
+  success: boolean;
+
+  @Field(() => Action)
+  action: Action;
+
+  @Field(() => ID, { nullable: true })
+  id: string;
+
+  @Field(() => Boolean, { nullable: true })
+  title: string;
+
+  @Field(() => [Image], { nullable: true })
+  images: Image[];
+
+  @Field(() => Boolean, { nullable: true })
+  isCtxUserIdAFollowerOfPostUser?: boolean;
+
+  @Field(() => User, { nullable: true })
+  user: User;
+
+  @Field(() => Date, { nullable: true })
+  created_at: Date;
+
+  @Field(() => Date, { nullable: true })
+  updated_at?: Date;
+}
 
 @Resolver()
 export class MyFollowingPostsResolver {
   @UseMiddleware(isAuth, logger)
+  @Subscription(() => HandlePostPayload, {
+    // @ts-ignore
+    topics: ({ context }: any) => {
+      if (!context.userId) {
+        throw new Error("not authorized for this topic");
+      }
+
+      return "GLOBAL_POSTS";
+    },
+
+    // @ts-ignore
+    filter: ({
+      payload,
+      args
+    }: ResolverFilterData<HandlePostPayload, PostSubscriptionInput>) => {
+      console.log("posts subscription".toUpperCase());
+      // filter for those ctx.userId is following
+      // Filter based on user? Not sure yet
+
+      return true;
+    }
+  })
+  followingPostsSub(
+    @Root() feedPayload: HandlePostPayload,
+    @Ctx() ctx: MyContext
+    // @Arg("data", () => PostSubscriptionInput)
+    // input: PostSubscriptionInput
+  ): HandlePostPayload {
+    // opportunity to transform data (the filter above has to be boolean)
+    console.log("VIEW THE PAYLOAD", util.inspect(feedPayload, true, 3, true));
+    feedPayload.isCtxUserIdAFollowerOfPostUser &&
+      feedPayload.user.id === ctx.userId;
+
+    return feedPayload; // createdAt: new Date()
+  }
+
   @Query(() => [Post], {
     name: "myFollowingPosts",
     nullable: true
@@ -19,6 +139,8 @@ export class MyFollowingPostsResolver {
   ): Promise<any> {
     const userId = ctx.req.session ? ctx.req.session.userId : null;
 
+    // search for the logged in user
+    // get posts of those the user follows
     let thoseIFollowAndTheirPosts = await User.findOne({
       where: { id: userId },
       relations: [
