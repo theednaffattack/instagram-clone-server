c81403f06982d3de9b5e0944e8e18332c4047b94
eddie naff <eddienaff@eddienaff.com>
2019-11-12 16:17:02 -0800

    Add likes count subscription

---
+++ src/modules/posts/likes/likesCount.subscription.resolver.ts
@@ -0,0 +1,64 @@
+import {
+  Resolver,
+  ResolverFilterData,
+  // Root,
+  Subscription,
+  Arg,
+  InputType,
+  Field,
+  ID,
+  ObjectType,
+  Int,
+  Root
+} from "type-graphql";
+
+import { Like } from "../../../entity/Like";
+// import { LikeStatus } from "./updateLikes.resolver";
+
+@ObjectType()
+export class LikesCountType {
+  @Field(() => Int)
+  count: number;
+
+  @Field(() => ID)
+  postId: string;
+}
+
+@InputType()
+export class LikesCountArgs {
+  @Field(() => ID)
+  postId: string;
+}
+
+@Resolver()
+export class LikesCountResolver {
+  @Subscription(() => LikesCountType, {
+    topics: ({ context }: any) => {
+      if (!context.userId) {
+        throw new Error("Not authorized for this topic");
+      }
+
+      return "COUNT_UPDATED";
+    },
+
+    filter: ({ payload, args }: ResolverFilterData<LikesCountType, any>) => {
+      // @ts-ignore
+      return payload.postId === args.input.postId;
+    }
+  })
+  async likesCount(
+    @Root() likesCountPayload: LikesCountType,
+    @Arg("input") input: LikesCountArgs
+  ): Promise<LikesCountType> {
+    // @ts-ignore
+    let [data, count] = await Like.findAndCount({
+      where: { post: { id: input.postId } }
+    });
+    // console.log("WHAT THE HELL IS GOING ON?", {
+    //   count,
+    //   data,
+    //   likesCountPayload
+    // });
+    return { count: likesCountPayload.count, postId: likesCountPayload.postId };
+  }
+}
