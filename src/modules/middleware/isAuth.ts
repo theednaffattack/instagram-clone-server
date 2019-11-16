9eb1a7e5615e79aaf93c39fdc59ab175d3e48dee
eddie naff <eddienaff@eddienaff.com>
2019-11-15 16:24:03 -0800

    Remove useless else condition

--- src/modules/middleware/isAuth.ts
+++ src/modules/middleware/isAuth.ts
@@ -4,9 +4,6 @@ import { MyContext } from "../../types/MyContext";
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   if (!context.req.session!.userId && !context.userId) {
     throw new Error("Not authenticated");
-  } else {
-    // const { userId } = context.req.session;
-    // const userId = context.req.session ? context.req.session.userId : null;
   }
   return next();
 };
