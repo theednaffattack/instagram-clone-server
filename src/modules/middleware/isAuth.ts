6a378f18e7395f89e9ba4ba37b9933be88dfddfb
Eddie Naff <eddie@eddienaff.dev>
2020-10-14 13:29:54 -0700

    Adjust auth middleware

--- src/modules/middleware/isAuth.ts
+++ src/modules/middleware/isAuth.ts
@@ -2,7 +2,8 @@ import { MiddlewareFn } from "type-graphql";
 import { MyContext } from "../../types/MyContext";
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  if (!context.req.session!.userId && !context.userId) {
+  // if (!context.req.session!.userId && !context.userId) {
+  if (!context.userId) {
     throw new Error("Not authenticated");
   }
   return next();
