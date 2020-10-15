bb1cb32200d70a165f4df316cc928e3c5c44d908
Eddie Naff <eddie@eddienaff.dev>
2020-10-15 14:04:34 -0700

    Remove firstName and lastName from inputs

--- src/modules/user/register/EditUserInput.ts
+++ src/modules/user/register/EditUserInput.ts
@@ -1,16 +1,19 @@
-import { Length, IsEmail } from "class-validator";
+import { IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
 // import { PasswordInput } from "../../shared/PasswordInput";
 
 @InputType()
 export class EditUserInput {
-  @Field()
-  @Length(1, 255)
-  firstName: string;
+  // @Field()
+  // @Length(1, 255)
+  // firstName: string;
+
+  // @Field()
+  // @Length(1, 255)
+  // lastName: string;
 
   @Field()
-  @Length(1, 255)
-  lastName: string;
+  username: string;
 
   @Field()
   @IsEmail()
--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,22 +1,25 @@
-import { Length, IsEmail } from "class-validator";
+import { IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
 import { IsEmailAlreadyExist } from "./isEmailAlreadyExists";
 import { PasswordInput } from "../../shared/PasswordInput";
 
 @InputType()
 export class RegisterInput extends PasswordInput {
-  @Field()
-  @Length(1, 255)
-  firstName: string;
+  // @Field()
+  // @Length(1, 255)
+  // firstName: string;
 
-  @Field()
-  @Length(1, 255)
-  lastName: string;
+  // @Field()
+  // @Length(1, 255)
+  // lastName: string;
 
   @Field()
   @IsEmail()
   @IsEmailAlreadyExist({ message: "email already in use" })
   email: string;
+
+  @Field()
+  username: string;
 
   @Field()
   termsAndConditions: boolean;
