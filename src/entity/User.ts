1fd1ab440b5ab11292798b09c7f946602da8239f
Eddie Naff <eddie@eddienaff.dev>
2020-10-15 14:02:42 -0700

    Remove firstname and lastname from entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,10 +5,10 @@ import {
   Column,
   OneToMany,
   ManyToMany,
-  JoinTable
+  JoinTable,
   // RelationCount
 } from "typeorm";
-import { Field, ID, ObjectType, Root } from "type-graphql";
+import { Field, ID, ObjectType } from "type-graphql";
 
 import { Post } from "./Post";
 import { Image } from "./Image";
@@ -47,85 +47,89 @@ export class User extends BaseEntity {
   @Column("int", { default: 0 })
   count: number;
 
-  @Field()
-  @Column()
-  firstName: string;
+  // @Field()
+  // @Column()
+  // firstName: string;
 
   @Field(() => [Message])
   mappedMessages: Message[];
 
-  @Field()
-  @Column()
-  lastName: string;
+  // @Field()
+  // @Column()
+  // lastName: string;
 
   @Field()
   @Column("text", { unique: true })
   email: string;
 
+  @Field()
+  @Column("text", { unique: true })
+  username: string;
+
   @Field(() => [Thread], { nullable: true })
-  @OneToMany(() => Thread, thread => thread.user)
+  @OneToMany(() => Thread, (thread) => thread.user)
   threads: Thread[];
 
   @Field(() => [Like], { nullable: true })
-  @OneToMany(() => Like, like => like.user)
+  @OneToMany(() => Like, (like) => like.user)
   likes: Like[];
 
-  @Field()
-  name(@Root() parent: User): string {
-    return `${parent.firstName} ${parent.lastName}`;
-  }
+  // @Field()
+  // name(@Root() parent: User): string {
+  //   return `${parent.firstName} ${parent.lastName}`;
+  // }
 
   @Column()
   password: string;
 
   // @ts-ignore
-  @Field(type => Boolean)
+  @Field((type) => Boolean)
   @Column("bool", { default: false })
   confirmed: boolean;
 
   // @ts-ignore
-  @Field(type => Post, { nullable: true })
-  @OneToMany(() => Post, post => post.user)
+  @Field((type) => Post, { nullable: true })
+  @OneToMany(() => Post, (post) => post.user)
   posts?: Post[];
 
   // @ts-ignore
-  @Field(type => Image, { nullable: "itemsAndList" })
-  @OneToMany(() => Image, image => image.user, { nullable: true })
+  @Field((type) => Image, { nullable: "itemsAndList" })
+  @OneToMany(() => Image, (image) => image.user, { nullable: true })
   images: Image[];
 
   // @ts-ignore
-  @Field(type => String, { nullable: true })
+  @Field((type) => String, { nullable: true })
   @Column("varchar", { nullable: true })
   profileImgUrl?: string;
 
   // @ts-ignore
-  @Field(type => [Message], { nullable: true })
-  @OneToMany(() => Message, message => message.user)
+  @Field((type) => [Message], { nullable: true })
+  @OneToMany(() => Message, (message) => message.user)
   messages?: Message[];
 
   // @ts-ignore
-  @Field(type => [Message], { nullable: true })
-  @OneToMany(() => Message, message => message.sentBy)
+  @Field((type) => [Message], { nullable: true })
+  @OneToMany(() => Message, (message) => message.sentBy)
   sent_messages: Message[];
 
   // @ts-ignore
-  @Field(type => [User], { nullable: "itemsAndList" })
+  @Field((type) => [User], { nullable: "itemsAndList" })
   // @ts-ignore
-  @ManyToMany(type => User, user => user.following, { nullable: true })
+  @ManyToMany((type) => User, (user) => user.following, { nullable: true })
   @JoinTable()
   followers: User[];
 
   // @ts-ignore
-  @Field(type => [Thread], { nullable: "itemsAndList" })
+  @Field((type) => [Thread], { nullable: "itemsAndList" })
   // @ts-ignore
-  @ManyToMany(type => Thread, thread => thread.invitees, { nullable: true })
+  @ManyToMany((type) => Thread, (thread) => thread.invitees, { nullable: true })
   @JoinTable()
   thread_invitations: Thread[];
 
   // @ts-ignore
-  @Field(type => [User], { nullable: "itemsAndList" })
+  @Field((type) => [User], { nullable: "itemsAndList" })
   // @ts-ignore
-  @ManyToMany(type => User, user => user.followers, { nullable: true })
+  @ManyToMany((type) => User, (user) => user.followers, { nullable: true })
   following: User[];
 
   // @RelationCount((user: User) => user.followers)
