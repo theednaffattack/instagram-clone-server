e86b42818f19e406b9409be31e28a26447c57b7b
Eddie Naff <eddie@eddienaff.dev>
2020-10-17 20:54:44 -0700

    Add cookie name to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
 export const redisSessionPrefix = "sess:";
 export const userSessionIdPrefix = "userSids";
 export const forgotPasswordPrefix = "forgotPassword";
+export const COOKIE_NAME = process.env.COOKIE_NAME
+  ? process.env.COOKIE_NAME
+  : "COOKIE_NAME is not defined.";
