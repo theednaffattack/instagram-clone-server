235e11652d28b03545ada8a7381b9c0f7cf99de1
Eddie Naff <eddie@eddienaff.dev>
2020-10-17 21:04:34 -0700

    Add dev host to constants (confifg)

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,7 @@ export const forgotPasswordPrefix = "forgotPassword";
 export const COOKIE_NAME = process.env.COOKIE_NAME
   ? process.env.COOKIE_NAME
   : "COOKIE_NAME is not defined.";
+
+export const DEVELOPMENT_HOST = process.env.DEVELOPMENT_HOST
+  ? process.env.DEVELOPMENT_HOST
+  : "COOKIE_NAME is not defined.";
