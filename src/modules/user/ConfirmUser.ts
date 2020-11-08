26df68e5059aabf52dc2f48e0f6aaab6faada6ea
Eddie Naff <eddie@eddienaff.dev>
2020-11-08 13:51:55 -0800

    Add error handling

--- src/modules/user/ConfirmUser.ts
+++ src/modules/user/ConfirmUser.ts
@@ -3,25 +3,40 @@ import { Arg, Resolver, Mutation } from "type-graphql";
 import { redis } from "../../redis";
 import { User } from "../../entity/User";
 import { confirmUserPrefix } from "../constants/redisPrefixes";
+// import { MyContext } from "../../types/MyContext";
 
 @Resolver()
 export class ConfirmUserResolver {
   @Mutation(() => Boolean)
-  async confirmUser(
-    @Arg("token") token: string
-    // @Ctx() ctx: MyContext
-  ): Promise<boolean> {
-    const userId = await redis.get(confirmUserPrefix + token);
+  async confirmUser(@Arg("token") token: string): Promise<boolean> {
+    let userId;
+    try {
+      userId = await redis.get(confirmUserPrefix + token);
+    } catch (error) {
+      console.warn("USER CONFIRMATION", error);
+    }
 
     if (!userId) {
       return false;
     }
 
-    // update the user to be confirmed and ditch the token from redis
-    // @todo: prefix ALL redis tokens for readability
-    await User.update({ id: userId }, { confirmed: true });
-    await redis.del(token);
+    // Update the user to be confirmed and remove the token from redis
+    try {
+      const response = await User.update({ id: userId }, { confirmed: true });
+    } catch (error) {
+      throw Error(`User confirmation error:\n${error}`);
+    }
 
+    try {
+      await redis.del(confirmUserPrefix + token);
+    } catch (error) {
+      throw Error(`User token deletion error:\n${error}`);
+    }
+
+    // all is well return the user we found
+
+    // ctx.req.session!.userId = userId;
+    // console.log("VIEW CONFIRM USER RESPONSE", { userId, ctx: ctx.req.session });
     return true;
   }
 }
