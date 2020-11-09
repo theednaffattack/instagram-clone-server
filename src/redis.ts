6b877fa55297a9cd290f56249f113b0a7f4fd4d6
Eddie Naff <eddie@eddienaff.dev>
2020-11-09 14:59:35 -0800

    Adapt redis config to new environment

--- src/redis.ts
+++ src/redis.ts
@@ -6,26 +6,26 @@ const nodeEnvIs_NOT_Prod = process.env.NODE_ENV !== "production";
 const homeOptions: Redis.RedisOptions = {
   host: "localhost",
   port: 6379,
-  retryStrategy: (times: any) => Math.max(times * 100, 3000)
+  retryStrategy: (times: any) => Math.max(times * 100, 3000),
 };
 
 const prodOptions: Redis.RedisOptions = {
-  host: "pearlfish.redistogo.com",
-  port: 9963,
-  name: "redistogo",
-  password: "ddaf1c87a85195830ed43c6150b15a71",
-  retryStrategy: (times: any) => Math.max(times * 100, 3000)
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT_NUMBER!, 10),
+  name: "myredis",
+  password: process.env.REDIS_PASSWORD!,
+  retryStrategy: (times: any) => Math.max(times * 100, 3000),
 };
 
 // create Redis-based pub-sub
 export const pubSub = nodeEnvIs_NOT_Prod
   ? new RedisPubSub({
       publisher: new Redis(homeOptions),
-      subscriber: new Redis(homeOptions)
+      subscriber: new Redis(homeOptions),
     })
   : new RedisPubSub({
       publisher: new Redis(prodOptions),
-      subscriber: new Redis(prodOptions)
+      subscriber: new Redis(prodOptions),
     });
 
 export const redis = nodeEnvIs_NOT_Prod
